a9afc00155181ee50738e2d56995aef61c366d28
drewhermanson <97925194+drewhermanson@users.noreply.github.com>
2024-03-28 15:12:13 -0500

    forgot to add comments on strategies

--- strategies/local.js
+++ strategies/local.js
@@ -2,6 +2,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const passport = require('passport');
 const db = require('../models');
 
+//this is the strategy used in the routes to validate that the user is in the database and then creates the req.user object
 passport.use(new LocalStrategy(
     async function(username, password, done) {
         const user = await db.User.findOne({ where: { username: username } });
@@ -25,6 +26,7 @@ passport.deserializeUser(async function(id, done) {
 });
 
 module.exports = passport;
+
 
 //this page is for setting up passport authentication. 
 //Have no actual idea if this works yet but i followed a tutorial and this is what they did.
